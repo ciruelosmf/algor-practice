8b82c93909da4e70ed28195e20622d0c440ffd9c
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2021-12-29 23:51:30 +0000

    solution added, not mine

--- pascalInteger.py
+++ pascalInteger.py
@@ -1,23 +1,39 @@
-# given intenger numberRows, return first row of P traigle
+# given intenger numberRows, return first numberRows of P traigle
 # example: numberRows = 5 -> 
-#        [1] 
-#       [1, 1],
-#      [1, 2, 1], 
-#     [1, 3, 3, 1]
-#    [1, 4, 6, 4, 1]
+#         [ 1 ] 
+#        [1, 1]
+#       [1, 2, 1]
+#      [1, 3, 3, 1]
+#    [ 1, 4, 6, 4, 1 ]
+
+
 
 class Solution:
+  def numberRowsPascalTriang(self, numRows:int):
+    res = [[1]]
+    for i in range(numRows-1):
+      temp = [0] + res[-1] + [0]
+      row = []
+      for j in range(len(res[-1]) + 1):
+        row.append(temp[j] + temp[j+1])
+      res.append(row)
+    print(res)
+
+
+
+"""
+class Solution:
   def numberRowsPascalTriang(self, rows:int) -> list:
-    rows = []
+    rowLists = []
     rows2 = [] 
     aux = 0
     aux2 = 0
-    for i in range(0, len(listt)-1):
-      for z in range(i, len(listt)):
-        lst2.append(listt[z] * listt[i] )
-    result = [i for i in lst2 if i in listt]
-    print(lst2)
-    if len(result) != 0:
-      print("este es", max(result))
-    else:
-      print(" N/A")
+    for i in range(rows):
+      rowLists.append(1)
+      print(rowLists)
+      rows2.append(list(rowLists))
+    print(rows2, "hola")
+"""
+
+a = Solution()
+a.numberRowsPascalTriang(6)
