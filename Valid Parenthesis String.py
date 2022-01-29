62711cee519e606d6a116e175ae24f6e3d93a9bb
ciruelosmf <62398984+ciruelosmf@users.noreply.github.com>
2022-01-29 18:58:24 -0300

    Create Valid Parenthesis String.py
    1

---
+++ Valid Parenthesis String.py
@@ -0,0 +1,22 @@
+string_to_check = "(*)*()"
+
+
+
+
+
+
+def valid_parenthesis_string(stringy):
+    minLeft = 0
+    maxLeft = 0
+
+    for c in stringy:
+        if i + a[i] >= target:
+            print(i + a[i],"asd")
+            target = i
+        print(i,"i",target)
+        print(" ")
+
+
+b = valid_parenthesis_string(stringy)
+
+print(b, "valid_parenthesis_string")
